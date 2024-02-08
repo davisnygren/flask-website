a21fc3421f7c2b9dd4102a90cf5756732da5ecca
Davis Nygren <davisnygren@users.noreply.github.com>
2024-02-08 11:55:36 -0600

    Added Flask-Moment for times on user.html and _post.html

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -12,6 +13,7 @@ app = Flask (__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
